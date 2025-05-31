131b7dac912b505abbd42aaa399ec6e3bdad8db8
Kizhoo <lukasvalentino30@gmail.com>
2025-06-01 06:26:35 +0700

    Update search.js

--- src/api/search.js
+++ src/api/search.js
@@ -1,5 +1,5 @@
 // Konfigurasi Google Custom Search API
-const GOOGLE_API_KEY = 'AIzaSyCWja1Z5bqT_-Vy_J_hxWMb_Da0pjkrzvk';
+const GOOGLE_API_KEY = 'AIzaSyCjZ-dY6hXvYm7Cbl20_OpQHI3NSIovBWs';
 const GOOGLE_CSE_ID = '234faf42b08954785';
 const GOOGLE_API_URL = 'https://www.googleapis.com/customsearch/v1';
 
